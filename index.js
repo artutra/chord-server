72355517fb7fa12583f8b8bcef41dcf640dbcdec
Artur Miranda <ldm.artur@gmail.com>
2017-05-20 23:14:37 -0300

    Add site url to env

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 // Add headers
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8000');
+    res.setHeader('Access-Control-Allow-Origin', process.env.SITE_URL);
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     // Request headers you wish to allow
